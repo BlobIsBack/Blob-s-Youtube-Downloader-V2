f7edbd432fc8c26f336acf72cc1c236eb7620568
BlobIsBack <linkedparsley@gmail.com>
2024-01-31 13:39:09 +0100

    Made the code more readable

--- Downloader.py
+++ Downloader.py
@@ -2,6 +2,10 @@ from yt_dlp import YoutubeDL
 import os
 import customtkinter as customtk
 
+# Blob's Youtube Downloader
+# Le logiciel peut surement être utilisé pour télécharger sur d'autres sites mais je n'ai pas testé
+
+
 # Configuration de la fenetre
 customtk.set_appearance_mode("Dark")
 customtk.set_default_color_theme("blue")
@@ -16,37 +20,52 @@ if not os.path.exists(os.path.join(project_folder, 'Downloaded')):
     os.mkdir(os.path.join(project_folder, 'Downloaded'))
 output_folder = os.path.join(project_folder, 'Downloaded')
 
-ydl_ops = {
+
+# Configuration du telechargement mp3
+mp3dl_opts = {
     'format': 'bestaudio[ext=mp3]/best',
     'outtmpl': os.path.join(output_folder, '%(title)s.mp3'),
 }
 
-dl_ops = {
+# Configuration du telechargement mp4
+mp4dl_opts = {
+    'format': 'best[ext=mp4]/best',
     'outtmpl': os.path.join(output_folder, '%(title)s.mp4'),
 }
 
+
+# Configuration de la boite de texte + activation de la boite de texte (pour mettre le lien)
 textbox = customtk.CTkTextbox(app, width=355, height=20 , corner_radius=10)
 textbox.place(relx=0.5, anchor="center", rely=0.5)
 
+
+# Message pour dire quoi faire en cas de probleme
 URLlabel = customtk.CTkLabel(master=app, text="URL de la vidéo :")
 label = customtk.CTkLabel(master=app, text="Si le telechargement ne fonctionne pas, \n verifiez votre connexion internet ainsi que l'URL de la vidéo \n puis redémarrez le logiciel")
 URLlabel.place(relx=0.5, anchor="center", y=120)
 label.place(relx=0.5, anchor="center", y=40)
 
+
+# Fonction de telechargement mp4
 def DownloadMp4():
     URLS = textbox.get("1.0", "end")
-    with YoutubeDL(dl_ops) as ydl:
+    with YoutubeDL(mp4dl_opts) as ydl:
         ydl.download(URLS)
 
+# Fonction de telechargement mp3
 def DownloadMp3():
     URLS = textbox.get("1.0", "end")
-    with YoutubeDL(ydl_ops) as ydl:
+    with YoutubeDL(mp3dl_opts) as ydl:
         ydl.download(URLS)
 
+
+# Mise en place et activation des boutons
 buttonmp4 = customtk.CTkButton(app, text="Telecharger en tant que MP4",width=70, height=25, command=DownloadMp4)
 buttonmp4.place(relx=0.27, anchor="center", rely=0.63)
 
 buttonmp3 = customtk.CTkButton(app, text="Telecharger en tant que MP3",width=70, height=25, command=DownloadMp3)
 buttonmp3.place(relx=0.73, anchor="center", rely=0.63)
 
+
+# Lancement de l'application
 app.mainloop()
